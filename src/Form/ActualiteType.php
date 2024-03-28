0238b0fc75eea92b568655d7969c90c9dfea434f
Helmi Gargouri <helmi.gargouri1@gmail.com>
2024-03-28 18:11:56 +0100

    add Crud Actualite

---
+++ src/Form/ActualiteType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Actualite;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ActualiteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('text')
+            ->add('date')
+            ->add('image')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Actualite::class,
+        ]);
+    }
+}
