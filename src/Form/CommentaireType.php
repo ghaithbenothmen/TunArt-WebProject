c69856e1f1d6c03252213c87f90db9eb2c9a6849
Helmi Gargouri <helmi.gargouri1@gmail.com>
2024-03-28 18:12:32 +0100

    add Crud Commentaire

---
+++ src/Form/CommentaireType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commentaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('contenuc')
+            ->add('idAct')
+            ->add('idUser')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaire::class,
+        ]);
+    }
+}
