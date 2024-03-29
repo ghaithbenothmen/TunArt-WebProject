71aa91c7236059f25053dd62a4117cad82e29e53
Ben Othmen Ghaith <ghaithbenothmen8@gmail.com>
2024-03-29 04:02:22 +0100

    CRUD categorie

--- src/Controller/ArtisteController.php
+++ src/Controller/ArtisteController.php
@@ -2,8 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Form\CategorieFormType;
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Form\Test\FormInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArtisteController extends AbstractController
@@ -15,11 +23,104 @@ class ArtisteController extends AbstractController
             'controller_name' => 'ArtisteController',
         ]);
     }
-    #[Route('/liste', name: 'app_artiste_liste')]
-    public function liste(): Response
+    #[Route('/listeCat', name: 'app_artiste_listeCat')]
+    public function listeCat(CategorieRepository $repo): Response
     {
-        return $this->render('artiste/categorie.html.twig', [
-            'controller_name' => 'ArtisteController',
+        $categories = $repo->findAll();
+        $cat = new Categorie();
+        $form = $this->createForm(CategorieFormType::class, $cat);
+        return $this->render('artiste/categorie.html.twig', ['categories' => $categories, 'f' => $form->createView()]);
+    }
+
+
+    private function getErrorsFromForm(FormInterface $form): array
+    {
+        $errors = [];
+        foreach ($form->getErrors(true, true) as $error) {
+            $errors[] = $error->getMessage();
+        }
+
+        foreach ($form->all() as $childForm) {
+            if ($childErrors = $this->getErrorsFromForm($childForm)) {
+                $errors[$childForm->getName()] = $childErrors;
+            }
+        }
+
+        return $errors;
+    }
+
+    #[Route('/addCat', name: 'app_add_cat')]
+    public function addCat(Request $request, ManagerRegistry $manager): Response
+    {
+        $cat = new Categorie();
+        $form = $this->createForm(CategorieFormType::class, $cat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $manager->getManager()->persist($cat);
+                $manager->getManager()->flush();
+
+                return new JsonResponse(['success' => true]);
+            } catch (\Exception $e) {
+                return new JsonResponse(['success' => false, 'message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        $errors = $this->getErrorsFromForm($form);
+        return new JsonResponse(['success' => false, 'errors' => $errors], Response::HTTP_BAD_REQUEST);
+    }
+
+
+    #[Route('/categorie/delete/{id}', name: 'app_categorie_delete')]
+    public function deleteCat($id, ManagerRegistry $manager, CategorieRepository $repo)
+    {
+        $cat = $repo->find($id);
+        $manager->getManager()->remove($cat);
+        $manager->getManager()->flush();
+        return $this->redirectToRoute('app_artiste_listeCat');
+    }
+
+    #[Route('/getCat', name: 'app_get_cat')]
+    public function getCat(Request $request): JsonResponse
+    {
+        $id = $request->query->get('id');
+        $entityManager = $this->getDoctrine()->getManager();
+        $cat = $entityManager->getRepository(Categorie::class)->find($id);
+
+        if (!$cat) {
+            return new JsonResponse(['error' => 'Category not found'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        return new JsonResponse([
+            'id' => $cat->getId(),
+            'nom' => $cat->getNom(),
         ]);
     }
+
+  
+    
+    
+    #[Route('/updateCat/{id}', name: 'app_update_cat')]
+    public function updateCat(Request $request, int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $category = $entityManager->getRepository(Categorie::class)->find($id);
+
+        if (!$category) {
+            throw $this->createNotFoundException('Category not found');
+        }
+
+        $category->setNom($request->request->get('nom')); 
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_artiste_listeCat');
+    }
+    
+
+
+
+
+
 }
