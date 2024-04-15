9ef99a24e2d3c17887fa73e3460b620233aaf206
AZIZ <mohamedaziz.rihani@esprit.tn>
2024-04-15 04:28:34 +0100

    Concours CRUD

---
+++ src/Controller/ConcoursControllerFront.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ConcoursRepository;
+use App\Entity\Concours;
+use App\Form\ConcoursType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/concoursfront')]
+class ConcoursControllerFront extends AbstractController
+{
+    #[Route('/', name: 'app_concoursfront_index', methods: ['GET'])]
+    public function index(ConcoursRepository $concoursRepository): Response
+    {
+        return $this->render('concoursfront/indexfront.html.twig', [
+            'concours' => $concoursRepository->findAll(),
+        ]);
+    }
+
+
+}
