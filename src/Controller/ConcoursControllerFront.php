a6ce9ecced048c39f9a8ba31f8e07ead5741c750
Aziz Rihani <mohamedaziz.rihani@esprit.tn>
2024-04-27 00:05:47 +0100

    Mailing

--- src/Controller/ConcoursControllerFront.php
+++ src/Controller/ConcoursControllerFront.php
@@ -6,6 +6,8 @@ use App\Repository\ConcoursRepository;
 use App\Entity\Concours;
 use App\Form\ConcoursType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,5 +24,22 @@ class ConcoursControllerFront extends AbstractController
         ]);
     }
 
+    #[Route('/a', name: 'app_concoursfront_participate', methods: ['GET'])]
+    public function participate(ConcoursRepository $concoursRepository,MailerInterface $mailer): Response
+    {
+        $message = (new Email())
+        ->from('culturnaskapere@gmail.com')
+        ->to('aziz.rihani2002@gmail.com')
+        ->subject('Iscription concour Tunart')
+        ->html('<p>Vous avez effectuer une inscription dans un concour sur le platform de Tunart</p>')
+        ->text('Hello');
+
+    $mailer->send($message);
+
+    return $this->render('concoursfront/indexfront.html.twig', [
+        'concours' => $concoursRepository->findAll(),
+    ]);
+
+    }
 
 }
