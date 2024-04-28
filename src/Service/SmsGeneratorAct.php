9c82c68407ee17099ab26aae1472d1b4687a6b48
Helmi Gargouri <helmi.gargouri1@gmail.com>
2024-04-28 18:25:15 +0100

    Removed Twilio Account SID from .env and SmsGeneratorAct.php

--- src/Service/SmsGeneratorAct.php
+++ src/Service/SmsGeneratorAct.php
@@ -11,8 +11,8 @@ class SmsGeneratorAct
     {
         //AC193b508426a03f257f4fcb40dff97a95
         $accountSid ='';  //Identifiant du compte twilio
-        $authToken ='96d1e1088007780240afdd0f8c2e0413'; //Token d'authentification
-        $fromNumber ='+13346974956'; // Numéro de test d'envoie sms offert par twilio
+        $authToken ='';//96d1e1088007780240afdd0f8c2e0413'; //Token d'authentification
+        $fromNumber ='';//+13346974956'; // Numéro de test d'envoie sms offert par twilio
 
         $toNumber = $number; // Le numéro de la personne qui reçoit le message
         $message = ''.$name.' vous a envoyé le message suivant:'.' '.$text.''; //Contruction du sms
