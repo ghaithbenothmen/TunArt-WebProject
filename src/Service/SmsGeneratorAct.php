b2878d4330f3631b95e57a8b2b1456e3da190203
Helmi Gargouri <helmi.gargouri1@gmail.com>
2024-04-28 18:32:49 +0100

    add Twilio Account SID from .env and SmsGeneratorAct.php

--- src/Service/SmsGeneratorAct.php
+++ src/Service/SmsGeneratorAct.php
@@ -9,10 +9,10 @@ class SmsGeneratorAct
 
           public function SendSms(string $number, string $name, string $text)
     {
-        //AC193b508426a03f257f4fcb40dff97a95
-        $accountSid ='';  //Identifiant du compte twilio
-        $authToken ='';//96d1e1088007780240afdd0f8c2e0413'; //Token d'authentification
-        $fromNumber ='';//+13346974956'; // Numéro de test d'envoie sms offert par twilio
+
+        $accountSid ='AC193b508426a03f257f4fcb40dff97a95';  //Identifiant du compte twilio
+        $authToken ='96d1e1088007780240afdd0f8c2e0413'; //Token d'authentification
+        $fromNumber ='+13346974956'; // Numéro de test d'envoie sms offert par twilio
 
         $toNumber = $number; // Le numéro de la personne qui reçoit le message
         $message = ''.$name.' vous a envoyé le message suivant:'.' '.$text.''; //Contruction du sms
