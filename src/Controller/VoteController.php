68ee3f93f5f77143c25d1a651f87fa1a0d533118
AZIZ <mohamedaziz.rihani@esprit.tn>
2024-05-05 22:37:31 +0100

    Clean

---
+++ src/Controller/VoteController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\App\Repository\VoteRepository;
+use App\Entity\Vote;
+use App\Form\VoteType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/vote')]
+class VoteController extends AbstractController
+{
+    #[Route('/', name: 'app_vote_index', methods: ['GET'])]
+    public function index(VoteRepository $voteRepository): Response
+    {
+        return $this->render('vote/index.html.twig', [
+            'votes' => $voteRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_vote_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $vote = new Vote();
+        $form = $this->createForm(VoteType::class, $vote);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($vote);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_vote_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('vote/new.html.twig', [
+            'vote' => $vote,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{idVote}', name: 'app_vote_show', methods: ['GET'])]
+    public function show(Vote $vote): Response
+    {
+        return $this->render('vote/show.html.twig', [
+            'vote' => $vote,
+        ]);
+    }
+
+    #[Route('/{idVote}/edit', name: 'app_vote_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Vote $vote, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(VoteType::class, $vote);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_vote_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('vote/edit.html.twig', [
+            'vote' => $vote,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{idVote}', name: 'app_vote_delete', methods: ['POST'])]
+    public function delete(Request $request, Vote $vote, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$vote->getIdVote(), $request->request->get('_token'))) {
+            $entityManager->remove($vote);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_vote_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
--- src/Entity/Vote.php
+++ src/Entity/Vote.php
@@ -5,9 +5,9 @@ namespace App\Entity;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use DateTime;
-use App\Repository\CandidatureRepository;
+use App\Repository\VoteRepository;
 
-#[ORM\Entity(repositoryClass: App\Repository\CandidatureRepository::class)]
+#[ORM\Entity(repositoryClass: App\Repository\VoteRepository::class)]
 class Vote
 {
 
@@ -19,11 +19,59 @@ class Vote
     #[ORM\Column(type: 'date', length: 50)]
     private ?DateTime $date = null;
 
-    #[ORM\ManyToOne(inversedBy: 'Vote')]
-    private ?User $idUser = null;
+    #[ORM\ManyToOne(targetEntity: User::class ,inversedBy: 'Vote')]
+    #[ORM\JoinColumn(name: "idUser",referencedColumnName: "user")]
+    private ?User $user = null;
 
-    #[ORM\ManyToOne(inversedBy: 'Candidature')]
-    private ?Concours $idConcours = null;
+    #[ORM\ManyToOne(targetEntity: Concours::class , inversedBy: 'Vote')]
+    #[ORM\JoinColumn(name: "idConcours",referencedColumnName: "refrence")]
+    private ?Concours $Concours = null;
 
+    public function getIdVote(): ?int
+    {
+        return $this->idVote;
+    }
+    
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
 
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getIdConcours(): ?Concours
+    {
+        return $this->Concours;
+    }
+
+    public function setIdConcours(?Concours $Concours): static
+    {
+        $this->Concours = $Concours;
+
+        return $this;
+    }
+
+    public function getIdUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setIdUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function __construct(DateTime $currentDate,User $user,Concours $Concours)
+    {
+        $this->date = $currentDate;
+        $this->user = $user;
+        $this->Concours = $Concours;
+    }
 }
